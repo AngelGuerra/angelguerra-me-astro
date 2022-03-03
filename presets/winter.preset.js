9d4fe8af7deafd4fcb300191d311c56fc8a00313
Ángel Guerra <a.guerrarevilla@gmail.com>
2022-03-03 10:05:54 +0100

    Actualiza Astro y formatea los archivos
    - Utiliza el plugin de prettier de Astro.
    - Utiliza el plugin de prettier de Tailwind CSS.

--- presets/winter.preset.js
+++ presets/winter.preset.js
@@ -34,7 +34,8 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(ellipse at bottom, var(--tw-gradient-stops))",
+        "gradient-radial":
+          "radial-gradient(ellipse at bottom, var(--tw-gradient-stops))",
       },
       keyframes: winterAnimations
         .map((config) => config.keyframes)
